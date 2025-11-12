3a2902fd101436a50c81595cbc7b0ba859e75d96
xalanz <bubuthomas708@gmail.com>
2025-11-12 18:09:48 -0300

    información de equipo listas falta carito porductos

--- src/components/page/InformacioNostros.jsx
+++ src/components/page/InformacioNostros.jsx
@@ -1,13 +1,14 @@
 import React from 'react'
 import Contact from '../organisms/Contact'
 import Header from '../organisms/Header'
+import Informacion from '../organisms/Informacion'
 
 export default function Home() {
     return (
         <>
 
             <Header/>
-            
+            <Informacion/>
             <Contact/>
 
             

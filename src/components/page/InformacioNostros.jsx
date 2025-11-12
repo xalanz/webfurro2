91f134531cb1bf82b58fc9d84c84f6a0c3d2ccb1
xalanz <bubuthomas708@gmail.com>
2025-11-12 18:16:00 -0300

    erro areglaodo

--- src/components/page/InformacioNostros.jsx
+++ src/components/page/InformacioNostros.jsx
@@ -1,14 +1,14 @@
 import React from 'react'
 import Contact from '../organisms/Contact'
 import Header from '../organisms/Header'
-import Informacion from '../organisms/Informacion'
+import Nosotros from '../organisms/Nosotros'
 
 export default function Home() {
     return (
         <>
 
             <Header/>
-            <Informacion/>
+            <Nosotros/>
             <Contact/>
 
             

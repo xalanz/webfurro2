3a900c9307ce42f5ea6f737420aace94fabd89b4
xalanz <bubuthomas708@gmail.com>
2025-11-19 20:52:14 -0300

    funciona

--- src/App.jsx
+++ src/App.jsx
@@ -2,9 +2,6 @@ import { BrowserRouter, Route, Routes } from 'react-router-dom'
 import Home from './components/page/Home'
 import Login from './components/page/Login'
 import Registro from './components/page/Registro'
-import Tienda from './components/page/Tienda'
-import { Link } from 'react-router-dom'
-
 import { Link } from 'react-router-dom'
 import InformacioNostros from './components/page/InformacioNostros'
 import Carito from './components/page/Carito'
@@ -15,18 +12,17 @@ function App() {
 
   return (
     <BrowserRouter>
-    
-      <Link to="/"></Link>
+      <Link to="/Home"></Link>
       <Link to="/login" ></Link>
       <Link to="/registro"></Link>
       <Link to="/InformacioNostros"></Link>
-      <Link to="/tienda"></Link>
-    
+      <Link to="/Productos"></Link>
       <Routes>
         <Route path='/' element={<Home/>} />
+        <Route path='/Home' element={<Home/>} />
+        <Route path='/Productos' element={<Carito/>} />
         <Route path='/login' element={<Login/>} />
         <Route path='/registro' element={<Registro/>} />
-        <Route path='/tienda' element={<Tienda/>} />
         <Route path='/InformacioNostros' element={<InformacioNostros/>} />
       </Routes>
     </BrowserRouter>
--- src/components/organisms/Header.jsx
+++ src/components/organisms/Header.jsx
@@ -4,19 +4,19 @@ import { Link } from 'react-router-dom';
 export default function Header() {
 return (
     <header className="site-header">
-        <nav className="nav container">
-            <div className="logo">🧁 DulceLobito</div>
-            <ul className="nav-list">
-                
-                
-                <li><Link to="/Home">Inicio</Link></li>
-                <li><Link to="/login">Iniciar sesión</Link></li>
-                <li><Link to="/tienda">Productos</Link></li>
-                <li><Link to="/InformacioNostros">Nosotros</Link></li>
-                <li><Link to="/Contacto">Contacto</Link></li>
-
-            </ul>
-        </nav>
-    </header>
+    <nav className="nav container">
+    <div className="logo">🧁 DulceLobito</div>
+        <ul className="nav-list">
+        <li><Link to="/Home">inicio</Link></li>
+        <li><Link to="/login">Iniciar sesión</Link></li>
+        <li><Link to="/Productos">Productos</Link></li>
+        <li><Link to="/InformacioNostros">Nosotros</Link></li>
+        
+        
+    
+    </ul>
+    </nav>
+</header>
+    
     )
 }
